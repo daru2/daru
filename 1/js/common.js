fdc283a595a4c84fba63da7ffe787572c19a3f27
daru2 <60119901+daru2@users.noreply.github.com>
2020-02-06 17:22:31 +0900

    Add files via upload

--- 1/js/common.js
+++ 1/js/common.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
     $("#menu_btn").on("click", function(){
-        $(this).fadeOut(500);
+        $(this).fadeOut(300);
         // fadeOut이 아니라 사라지는 이유?
-        $("#menu_box").animate({ "left" : "0"}, 600);
+        $("#menu_box").animate({ "left" : "0"}, 300);
     });
     $("#menu_cls").on("click", function(){
-        $("#menu_box").animate({ "left" : "-60vw"}, 600);
-        $("#menu_btn").fadeIn(500);
+        $("#menu_box").animate({ "left" : "-60vw"}, 300);
+        $("#menu_btn").fadeIn(300);
     });
 });
--- 1/js/index.js
+++ 1/js/index.js
@@ -1,6 +1,68 @@
+
 $(document).ready(function(){
+        /* 1page scroll */
+
+        $(".page").each(function (index) {
+            // 개별적으로 Wheel 이벤트 적용
+            $(this).on("mousewheel DOMMouseScroll", function (e) {
+                e.preventDefault();
+                var delta = 0;
+                if (!event) event = window.event;
+                if (event.wheelDelta) {
+                    delta = event.wheelDelta / 120;
+                    if (window.opera) delta = -delta;
+                } 
+                else if (event.detail)
+                    delta = -event.detail / 3;
+                var moveTop = $(window).scrollTop();
+                var elmSelecter = $(".page").eq(index);
+                // 마우스휠을 위에서 아래로
+                if (delta < 0) {
+                    if ($(elmSelecter).next() != undefined) {
+                        try{
+                            moveTop = $(elmSelecter).next().offset().top;
+                        }catch(e){}
+                    }
+                // 마우스휠을 아래에서 위로
+                } else {
+                    if ($(elmSelecter).prev() != undefined) {
+                        try{
+                            moveTop = $(elmSelecter).prev().offset().top;
+                        }catch(e){}
+                    }
+                }
+                 
+                // 화면 이동 0.5초(500)
+                $("html,body").stop().animate({
+                    scrollTop: moveTop + 'px'
+                }, {
+                    duration: 500, complete: function () {
+                    }
+                });
+            });
+        });
+    
+        /* /1page scroll */
+
+        /* cnt 설정 */
+
+
+
+        // var winH = parseInt($(window).height());
+        // $(window).scroll(function(){
+        //     var cnt = parseInt($(this).scrollTop() / winH);
+        //     console.log(cnt);
+        //     if(cnt >= 1){
+        //         $("#vdo").pause(); 
+        //     }
+        // });
+        
+        
+        // 실패
+        /* /cnt 설정 */
+
     function nextAni(){
-        $(".sl_box1").not(":animated").animate({"margin-left" : "-100%"}, 1000, function(){
+        $(".sl_box1").not(":animated").animate({"margin-left" : "-100%"}, 500, function(){
             $(".sl_box1 li").eq(0).appendTo($(".sl_box1"));
             $(".sl_box1").css("margin-left", "0");
             $(".btn_box1 button").eq(0).appendTo(".btn_box1");
@@ -9,7 +71,7 @@ $(document).ready(function(){
         });
     }
     
-    var intv = setInterval( function(){ nextAni() }, 3000);
+    var intv = setInterval( function(){ nextAni() }, 4000);
 
 
 
@@ -23,6 +85,6 @@ $(document).ready(function(){
             }
         }
         nextAni();
-        intv = setInterval( function(){ nextAni(); }, 3000);
+        intv = setInterval( function(){ nextAni(); }, 4000);
     });
 });
